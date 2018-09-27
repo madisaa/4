5b50f05b4571685357a40cb8a5efb20bccb0d78e
madisaa <43490282+madisaa@users.noreply.github.com>
2018-09-27 13:23:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-});
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '-'
+const prefix = '!'
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -44,28 +15,28 @@ client.on('message', msg => {
  
  
 client.on('message', message => {
-var prefix = "-";
+var prefix = "!";
  
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
   if (message.author.id == '395462979115679755' ) return;
  
-if (message.content.startsWith(prefix + 'ply) {
+if (message.content.startsWith(prefix + 'pl')) {
   client.user.setGame(argresult);
     message.channel.sendMessage(`**${argresult}** : تم تغيير الحالة`)
 } else
  
-if (message.content.startsWith(prefix + 'st)) {
+if (message.content.startsWith(prefix + 'st')) {
   client.user.setGame(argresult, "https://www.twitch.tv/sytra_ayman");
     message.channel.sendMessage(`**${argresult}** :تم تغيير الحالة الى ستريمنج`)
 } else
  
-if (message.content.startsWith(prefix + 'setwatch')) {
+if (message.content.startsWith(prefix + 'wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 } else
-if (message.content.startsWith(prefix + 'ls)) {
+if (message.content.startsWith(prefix + 'ls')) {
 client.user.setActivity(argresult, {type:'LISTENING'});
     message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
 } else
@@ -81,4 +52,3 @@ if (message.content.startsWith(prefix + 'setavt')) {
     message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
 }
 });
-client.login(process.env.BOT_TOKEN);
